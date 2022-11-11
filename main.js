4df35cfa58e3cae6304ecb8e56df466edea0b598
NathanaelBlackbourn <nathanaelblackbourn@gmail.com>
2022-11-11 11:09:36 +0100

    Fixed toggle elements function.

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-function toggleElements () {
-    arguments.array.forEach(element => {
+function toggleElements (...elements) {
+    elements.forEach(element => {
         element.classList.toggle("hidden");
     });
 }
@@ -8,8 +8,8 @@ function nav() {
     let goButton = document.getElementById("go");
     let welcome = document.getElementById("welcome");
     let navMain = document.getElementById("nav-main");
-    goButton.addEventListener("click", () => {
-        toggleElements( welcome, navMain);
+    goButton.addEventListener("click", (e) => {
+        toggleElements(welcome, navMain);
     });
 }
 
