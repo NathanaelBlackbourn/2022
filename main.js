45d30b0ca31735dca3c5bb20b2d89cd8afd0777b
NathanaelBlackbourn <nathanaelblackbourn@gmail.com>
2022-11-28 16:27:46 +0100

    Column toggling added.

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ const navElements = {};
 function runNav() {
     getNavElements();
     welcomeListener();
+    workListener();
+    projectsListener();
     aboutListener();
     CVListeners();
 }
@@ -17,10 +19,14 @@ function getNavElements() {
     // Columns to toggle
     navElements.welcome = document.getElementById('welcome');
     navElements.main = document.getElementById('nav-main');
+    navElements.workCol = document.getElementById('work-col');
+    navElements.projectsCol = document.getElementById('projects-col');
     navElements.aboutCol = document.getElementById('about-col');
     navElements.CVCol = document.getElementById('cv-col');
     // Buttons that toggle columns
     navElements.goButton = document.getElementById('go');
+    navElements.workButton = document.getElementById('work-button');
+    navElements.projectsButton = document.getElementById('projects-button');
     navElements.aboutButton = document.getElementById('about-me-button');
     // CV scroll buttons
     navElements.skillsButton = document.getElementById('skills-button');
@@ -35,26 +41,58 @@ function getNavElements() {
 }
 
 /**
- * Takes elements as arguments and toggles hidden state.
- * @param {HTMLElement} 1 or more, adds to array
+ * Checks arguments for class hidden, removes when found.
+ * @param  {...HTMLElement} elements Column elements to be shown.
  */
-function toggleElements (...elements) {
-    elements.forEach(element => {
-        element.classList.toggle("hidden");
-    });
+function toggleOn(...elements) {
+    elements.forEach(e => {
+        if (e.classList.contains('hidden')) {
+            e.classList.toggle('hidden');
+        }
+    })
+}
+
+/**
+ * Checks arguments for class hidden, adds where missing.
+ * @param  {...HTMLElement} elements Column elements to be hidden.
+ */
+function toggleOff(...elements) {
+    elements.forEach(e => {
+        if (e.classList.contains('hidden') === false) {
+            e.classList.toggle('hidden');
+        }
+    })
 }
 
 /** Adds event listener to welcome block. */
 function welcomeListener() {
     navElements.goButton.addEventListener("click", () => {
-        toggleElements(navElements.welcome, navElements.main);
+        toggleOn(navElements.main);
+        toggleOff(navElements.welcome);
     });
+}
+
+/** Adds event listener to work button */
+function workListener() {
+    navElements.workButton.addEventListener('click', () => {
+        toggleOn(navElements.workCol);
+        toggleOff(navElements.projectsCol, navElements.aboutCol, navElements.CVCol);
+    })
+}
+
+/** Adds event listener to projects button */
+function projectsListener() {
+    navElements.projectsButton.addEventListener('click', () => {
+        toggleOn(navElements.projectsCol);
+        toggleOff(navElements.workCol, navElements.aboutCol, navElements.CVCol);
+    })
 }
 
 /** Adds event listener to about me button */
 function aboutListener () {
     navElements.aboutButton.addEventListener('click', () => {
-        toggleElements(navElements.aboutCol, navElements.CVCol);
+        toggleOn(navElements.aboutCol, navElements.CVCol);
+        toggleOff(navElements.workCol, navElements.projectsCol);
         navElements.header.classList.toggle('expanded');
     })
 }
